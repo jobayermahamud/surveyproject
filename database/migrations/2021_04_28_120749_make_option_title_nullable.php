73215648bb94ed96e153e5fe8561605a14365b8f
jobayer <jobayermahamudkamalcse@gmail.com>
2021-04-28 18:10:05 +0600

    Make a column nullable

---
+++ database/migrations/2021_04_28_120749_make_option_title_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeOptionTitleNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('question_option', function (Blueprint $table) {
+            $table->string('option_title')->nullable($value = true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
